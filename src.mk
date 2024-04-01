f3adae1aad9c41fede20ca5a087d408c779b7067
Tyler Griggs <tgriggs@berkeley.edu>
2024-04-01 19:03:45 +0000

    debug build, thread local store

--- src.mk
+++ src.mk
@@ -236,7 +236,8 @@ LIB_SOURCES =                                                   \
   trace_replay/trace_replay.cc                                  \
   trace_replay/block_cache_tracer.cc                            \
   trace_replay/io_tracer.cc                                     \
-  util/async_file_reader.cc					                            \
+  util/tg_thread_local.cc					\
+  util/async_file_reader.cc					\
   util/build_version.cc                                         \
   util/cleanable.cc                                             \
   util/coding.cc                                                \
