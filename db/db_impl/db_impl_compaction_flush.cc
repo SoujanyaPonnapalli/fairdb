97145d8b5d87a4ea645b2341358df9b5c042236b
Tyler Griggs <tegriggs7@gmail.com>
2024-11-20 00:24:43 +0000

    debugging

--- db/db_impl/db_impl_compaction_flush.cc
+++ db/db_impl/db_impl_compaction_flush.cc
@@ -290,7 +290,7 @@ Status DBImpl::FlushMemTableToOutputFile(
   // TODO(tgriggs): Super hacky. Update code to use cfd->GetID()
   // auto& thread_metadata = TG_GetThreadMetadata();
   // if (cfd->GetName() == "default") {
-  //   thread_metadata.client_id = 1;
+  //   thread_metadata.client_id = 0;
   // } else {
   //   thread_metadata.client_id = std::stoi(cfd->GetName().substr(2));       
   // }
@@ -412,7 +412,7 @@ Status DBImpl::FlushMemTablesToOutputFiles(
   auto& thread_metadata = TG_GetThreadMetadata();
   std::string cf_name = bg_flush_args[0].cfd_->GetName();
   if (cf_name == "default") {
-    thread_metadata.client_id = 1;
+    thread_metadata.client_id = 0;
   } else {
     thread_metadata.client_id = std::stoi(cf_name.substr(2));       
   }
@@ -3410,7 +3410,7 @@ void DBImpl::BackgroundCallFlush(Env::Priority thread_pri) {
       job_context.Clean();
 
       // Reset the thread metadata.
-      TG_GetThreadMetadata().client_id = -2;
+      TG_GetThreadMetadata().client_id = -1;
 
       mutex_.Lock();
     }
