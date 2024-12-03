4cdfad39a5d68b7aace45d42966fe1814b7175f4
Dev Bali <devbali@berkeley.edu>
2024-12-03 10:04:58 +0000

    Monday night

--- include/rocksdb/advanced_cache.h
+++ include/rocksdb/advanced_cache.h
@@ -556,6 +556,8 @@ class Cache : public Customizable {
   // or destruction, guaranteed before or after any thread-shared operations.
   void SetEvictionCallback(EvictionCallback&& fn);
 
+  void* manager = nullptr;
+
  protected:
   std::shared_ptr<MemoryAllocator> memory_allocator_;
   EvictionCallback eviction_callback_;
